af3a73b2670cf9b0a08e78c60c3472956c2782ba
Mahasvan <mahasvan@gmail.com>
2024-06-09 23:25:07 +0530

    update variable names for shell.py

--- api/service/shell.py
+++ api/service/shell.py
@@ -1,41 +1,43 @@
-pink = "\033[95m"
-blue = "\033[94m"
-cyan = "\033[96m"
-green = "\033[92m"
-yellow = "\033[93m"
-red = "\033[91m"
-bold = "\033[1m"
-underline = "\033[4m"
-end_formatting = "\033[0m"
+PINK = "\033[95m"
+BLUE = "\033[94m"
+CYAN = "\033[96m"
+GREEN = "\033[92m"
+YELLOW = "\033[93m"
+RED = "\033[91m"
+BOLD = "\033[1m"
+UNDERLINE = "\033[4m"
+END_FORMATTING = "\033[0m"
+
+BASE = "    |--- "
 
 
-def print_pink_message(text):
-    print(f"{pink}        |--- {text}{end_formatting}")
+def print_pink_message(text, *args, **kwargs):
+    print(f"{PINK}{BASE}{text}{END_FORMATTING}", *args, **kwargs)
 
 
-def print_blue_message(text):
-    print(f"{blue}        |--- {text}{end_formatting}")
+def print_blue_message(text, *args, **kwargs):
+    print(f"{BLUE}{BASE}{text}{END_FORMATTING}", *args, **kwargs)
 
 
-def print_cyan_message(text):
-    print(f"{cyan}        |--- {text}{end_formatting}")
+def print_cyan_message(text, *args, **kwargs):
+    print(f"{CYAN}{BASE}{text}{END_FORMATTING}", *args, **kwargs)
 
 
-def print_green_message(text):
-    print(f"{green}        |--- {text}{end_formatting}")
+def print_green_message(text, *args, **kwargs):
+    print(f"{GREEN}{BASE}{text}{END_FORMATTING}", *args, **kwargs)
 
 
-def print_yellow_message(text):
-    print(f"{yellow}        |--- {text}{end_formatting}")
+def print_yellow_message(text, *args, **kwargs):
+    print(f"{YELLOW}{BASE}{text}{END_FORMATTING}", *args, **kwargs)
 
 
-def print_red_message(text):
-    print(f"{red}        |--- {text}{end_formatting}")
+def print_red_message(text, *args, **kwargs):
+    print(f"{RED}{BASE}{text}{END_FORMATTING}", *args, **kwargs)
 
 
 def format_bold(text):
-    return bold + text + end_formatting
+    return f"{BOLD}{text}{END_FORMATTING}"
 
 
 def format_underline(text):
-    return underline + text + end_formatting
+    return f"{UNDERLINE}{text}{END_FORMATTING}"
