28eddfa1da5777d2ca204b2063af24cb6ee0878e
Mahasvan <mahasvan@gmail.com>
2024-06-09 23:58:34 +0530

    make retriever a singleton class

--- api/service/retriever.py
+++ api/service/retriever.py
@@ -2,15 +2,26 @@ import chromadb
 
 from . import shell
 
+from typing import List
 
 class Retriever:
+    # make it a singleton
+    _instance = None
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(Retriever, cls).__new__(cls)
+        return cls._instance
+
     def __init__(self, chroma_collection: str, chroma_host: str = "localhost", chroma_port: int = 8000):
         self.chroma_server_ip = chroma_host
         self.chroma_port = chroma_port
         self.client = chromadb.HttpClient(host=chroma_host, port=chroma_port)
         shell.print_green_message(f"ChromaDB client connected to {chroma_host}")
-        self.chroma_collection = self.client.get_collection(chroma_collection)
+        self.chroma_collection = self.client.get_or_create_collection(chroma_collection)
         shell.print_green_message(f"ChromaDB collection loaded: {chroma_collection}")
+
+    def query(self, query_texts: List[str], n_results: int = 5):
+        return self.chroma_collection.query(query_texts=query_texts, n_results=n_results)
 
     def retrieve(self, query, n_results=5):
         response = self.chroma_collection.query(query_texts=[query], n_results=n_results)
